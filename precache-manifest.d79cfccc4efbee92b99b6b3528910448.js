1bf41d0cae011ffd110f6163c98123a396744e80
Anderson Madeira <andersonmadeiracs@gmail.com>
2020-08-16 15:51:01 -0300

    Updates

---
+++ precache-manifest.d79cfccc4efbee92b99b6b3528910448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a325a8069e1737ad47d5998d43abcfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1878467c05033ae524fb",
+    "url": "/static/css/main.d6a1918b.chunk.css"
+  },
+  {
+    "revision": "ccf2b1abfff3cd74a78a",
+    "url": "/static/js/2.f0d8e889.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f0d8e889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1878467c05033ae524fb",
+    "url": "/static/js/main.7b621eb8.chunk.js"
+  },
+  {
+    "revision": "556bf7ba23594d8290be",
+    "url": "/static/js/runtime-main.48005e88.js"
+  }
+]);
